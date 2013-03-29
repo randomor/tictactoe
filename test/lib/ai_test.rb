904b48bd32d3b2b4d7f0ff53af64660ffbe9e09c
Shaomeng Zhang <zhan0753@umn.edu>
2013-03-28 23:32:48 -0500

    added first test for ai

--- test/lib/ai_test.rb
+++ test/lib/ai_test.rb
@@ -6,5 +6,10 @@ module TTT
       @ai = AI.new
     end
 
+    def test_always_pick_winning_position_first
+      position = @ai.generate_next_move([SIDE_X,SIDE_X,0,SIDE_O,SIDE_O,0,0,0,0], SIDE_X)
+      assert_equal(3, position)
+    end
+
   end
 end
