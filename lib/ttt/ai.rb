2303dcde26ca2339788a291d8f753a6c5e47bc03
Shaomeng Zhang <zhan0753@umn.edu>
2013-03-29 11:08:26 -0500

    picking winning position always

--- lib/ttt/ai.rb
+++ lib/ttt/ai.rb
@@ -8,7 +8,35 @@ module TTT
 
     def generate_next_move(states, side)
       @states = states
-      states.index(0)+1
+      picked = states.index(0)+1
+      index = pick_winning_position 
+      picked = index+1 if index      
+    end
+
+    def pick_winning_index
+      states_array = @states.each_slice(3).to_a
+      states_array.each_with_index do |row, row_number|
+        column = states_array.transpose[row_number]
+        diagonal_line = [states_array[0][0], states_array[1][1], states_array[2][2]]
+        counter_diagonal_line = [states_array[0][2], states_array[1][1], states_array[2][0]]
+        if row.count(@side) == 2
+          blank_index = row.find_index(0)
+          return row_number*3 + blank_index
+        elsif column.count(@side) == 2
+          blank_index = column.find_index(0)
+          return blank_index*3 + row_number
+        elsif diagonal_line.count(@side) == 2
+          blank_index = diagonal_line.find_index(0)
+          return 4 if blank_index == 1
+          return 0 if blank_index == 0
+          return 8 if blank_index == 2
+        elsif counter_diagonal_line.count(@side) == 2
+          blank_index = counter_diagonal_line.find_index(0)
+          return 4 if blank_index == 1
+          return 2 if blank_index == 0
+          return 6 if blank_index == 2
+        end
+      end
     end
   end
 end
--- test/lib/ai_test.rb
+++ test/lib/ai_test.rb
@@ -7,8 +7,31 @@ module TTT
     end
 
     def test_always_pick_winning_position_first
+      skip
       position = @ai.generate_next_move([SIDE_X,SIDE_X,0,SIDE_O,SIDE_O,0,0,0,0], SIDE_X)
       assert_equal(3, position)
+      next_position = @ai.generate_next_move([SIDE_X,0,0,SIDE_X,SIDE_O,SIDE_O,0,0,0], SIDE_X)
+      assert_equal(7, next_position)
+    end
+
+    def test_pick_horizontal_winning_position
+      @ai.states = [SIDE_X,SIDE_X,0,SIDE_O,SIDE_O,0,0,0,0]
+      assert_equal(2, @ai.pick_winning_index)
+    end
+
+    def test_pick_vertical_winning_position
+      @ai.states = [SIDE_X,0,0,SIDE_X,SIDE_O,SIDE_O,0,0,0]
+      assert_equal(6, @ai.pick_winning_index)
+    end
+
+    def test_pick_diagnoal_winning_position
+      @ai.states = [SIDE_X,SIDE_O,SIDE_O,SIDE_O,0,SIDE_X,0,SIDE_O,SIDE_X]
+      assert_equal(4, @ai.pick_winning_index)
+    end
+
+    def test_pick_counter_diagnoal_winning_position
+      @ai.states = [0,0,SIDE_X,SIDE_O,SIDE_X,0,0,SIDE_O,SIDE_O]
+      assert_equal(6, @ai.pick_winning_index)
     end
 
   end
