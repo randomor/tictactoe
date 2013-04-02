cffbfd98ffb70d0fae514852239bbcbfe164e99a
Shaomeng Zhang <zhan0753@umn.edu>
2013-04-02 12:23:35 -0500

    added available positions for board

--- test/lib/board_controller_test.rb
+++ test/lib/board_controller_test.rb
@@ -14,6 +14,12 @@ module TTT
       assert_equal(@board_controller.game_status, :Playing)
     end
 
+    def test_available_positions
+      assert_equal([1, 2, 3, 4, 5, 6, 7, 8, 9], @board_controller.available_positions)
+      @board_controller.next_move(3)
+      assert_equal([1, 2, 4, 5, 6, 7, 8, 9], @board_controller.available_positions)
+    end
+
     def test_board_render
       #heredoc indent
       #http://rubyquicktips.com/post/4438542511/heredoc-and-indent
