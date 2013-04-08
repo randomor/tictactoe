591e8594899442ff903f7cadf8345d4dff9e1bfb
Shaomeng Zhang <zhan0753@umn.edu>
2013-04-08 09:28:42 -0500

    refactored board.rb, decompose long method

--- lib/ttt/board.rb
+++ lib/ttt/board.rb
@@ -45,25 +45,45 @@ module TTT
     end
 
     def winning_position_for_side(side)
-      rows.each_with_index do |current_row, row_number|
-        current_row.each_with_index do |state, column_number|
-          next if state != 0
-          current_column = columns[column_number]
-          if current_row.count(side) == 2 || current_column.count(side) == 2
-            return row_number*3 + column_number + 1
-          end
+      horizontal_winning_position_for_side(side) || vertical_winning_position_for_side(side) || diagonal_line_winning_position_for_side(side) || counter_diagonal_line_winning_position_for_side(side)
+    end
+
+    def counter_diagonal_line_winning_position_for_side(side)
+      if counter_diagonal_line.count(side) == 2 && counter_diagonal_line.count(0) == 1
+        blank_index = counter_diagonal_line.find_index(0)
+        return blank_index * 3 + (3 - blank_index)
+        # return 3 if blank_index == 0
+        # return 5 if blank_index == 1
+        # return 7 if blank_index == 2
+      end
+      return nil
+    end
+
+    def diagonal_line_winning_position_for_side(side)
+      if diagonal_line.count(side) == 2 && diagonal_line.count(0) == 1
+        blank_index = diagonal_line.find_index(0)
+        return blank_index * 3 + blank_index + 1
+        # return 1 if blank_index == 0
+        # return 5 if blank_index == 1
+        # return 9 if blank_index == 2
+      end
+      return nil
+    end
+
+    def vertical_winning_position_for_side(side)
+      columns.each_with_index do |current_column, column_number|
+        if current_column.count(side) == 2 && current_column.count(0) == 1
+          return column_number + current_column.find_index(0) * 3 + 1
         end
       end
-      if diagonal_line.count(side) == 2
-        blank_index = diagonal_line.find_index(0)
-        return 5 if blank_index == 1
-        return 1 if blank_index == 0
-        return 9 if blank_index == 2
-      elsif counter_diagonal_line.count(side) == 2
-        blank_index = counter_diagonal_line.find_index(0)
-        return 5 if blank_index == 1
-        return 3 if blank_index == 0
-        return 7 if blank_index == 2
+      return nil
+    end
+
+    def horizontal_winning_position_for_side(side)
+      rows.each_with_index do |current_row, row_number|
+        if current_row.count(side) == 2 && current_row.count(0) == 1
+          return row_number*3 + current_row.find_index(0) + 1
+        end
       end
       return nil
     end
